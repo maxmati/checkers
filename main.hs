754b3adb6d7d5579f15b4a9c26113932b30aa2aa
Mateusz  "Maxmati" Nowotyński <admin@maxmati.pl>
2016-05-11 09:53:22 +0200

    Switch board representation to Data.Map

--- main.hs
+++ main.hs
@@ -1,8 +1,10 @@
+import qualified Data.Map
+import qualified Control.Monad
+
 data Color = Black | White deriving Show
 data Type = Pawn | King deriving Show
 newtype Figure = Maybe (Type, Color) deriving Show
-type Row = [Figure]
-type Board = [Row]
+type Board = Data.Map.Map (Int, Int) Figure
 
 initBoard = ".b.b.b.b\nb.b.b.b.\n.b.b.b.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
 
@@ -10,8 +12,10 @@ charToField 'b' = Just (Pawn, Black)
 charToField 'w' = Just (Pawn, White)
 charToField a = Nothing
 
-stringToRow string = map charToField string
-stringToBoard string = map stringToRow (lines string)
+
+stringToRow string row = zip [(x,row)| x <- [1..8]] $ map charToField string
+stringToBoardList string = foldl (\all (row, string) -> all ++ (stringToRow string row)) [] $ zip [1..8] (lines string)
+stringToBoard string = Data.Map.fromList $ stringToBoardList string
 
 fieldToChar (Just (Pawn, Black)) = '♟'
 fieldToChar (Just (King, Black)) = '♛'
@@ -19,7 +23,11 @@ fieldToChar (Just (Pawn, White)) = '♙'
 fieldToChar (Just (King, White)) = '♕'
 fieldToChar Nothing = '.'
 
-rowToString row = map fieldToChar row
-boardToString board = unlines (map rowToString board)
+rowToString board row = foldl 
+				(\all k -> all ++ [fieldToChar (Control.Monad.join (Data.Map.lookup k board))] ) 
+				[] 
+				$ [(x, row) | x <- [1..8]]
+boardToString board = unlines (map (rowToString board) [1..8])
 
 putBoard board = putStr $  boardToString board
+
