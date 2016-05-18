f7ecf448ad012d8cb21860352e84a7f9b234ec9c
Mateusz  "Maxmati" Nowotyński <admin@maxmati.pl>
2016-05-18 08:57:16 +0200

    Beating

--- main.hs
+++ main.hs
@@ -39,11 +39,19 @@ boardToString board = unlines (map (rowToString board) [1..8])
 
 putBoard board = putStr $  boardToString board
 
+beatenPoses (fromX, fromY) (toX, toY) = let endX   = toX - fromX
+					    startX = signum endX 
+					    xMod   = if endX > startX then [startX..endX - 1] else [endX..startX + 1]
+					    endY   = toY - fromY
+					    startY = signum endY
+					    yMod   = if endY > startY then [startY..endY - 1] else [endY..startY + 1]
+					in [(fromX + x, fromY + y) | x <- xMod, y <- yMod, abs x == abs y ] 
+
 move board from to = let figure = fromJust $ Data.Map.lookup from board
-                     in  Data.Map.insert to figure $ Data.Map.delete from board
+                     in  Data.Map.insert to figure $ Data.Map.delete from $ foldr Data.Map.delete board $ beatenPoses from to
 
 generateMoves (fromX, fromY) color = let yMod = if color == Black then 1 else -1
-                                     in  [(fromX + xAdd, fromY + yAdd*yMod ) | xAdd <- [-2..2], yAdd <- [1..2], abs xAdd == yAdd]
+                                     in  [(fromX + xAdd, fromY + yAdd*yMod ) | xAdd <- [-2..2], yAdd <- [1..2], abs xAdd == yAdd] ++ [(fromX + xAdd, fromY - 2*yMod) | xAdd <- [-2,2]]
 
 removeOffBoard moves = let onBoard (x, y) = x > 0 && x <= 8 && y > 0 && y <= 8
                        in filter onBoard moves
@@ -51,15 +59,15 @@ removeOffBoard moves = let onBoard (x, y) = x > 0 && x <= 8 && y > 0 && y <= 8
 removeOccupied board moves = let isAvailable pos = Data.Map.lookup pos board == Nothing
                              in  filter isAvailable moves
 
-removeReqBeating board color from moves = let haveToBeat (x, y) = abs (x - fst from) == 2
-                                              yMod = if color == Black then 1 else -1
-                                              beatPos (x, y) = if x > fst from then (x - 1, y - yMod)
-                                                                               else (x + 1, y - yMod)
-                                              unableToBeat pos = case (Data.Map.lookup (beatPos pos) board) of Nothing -> True
-                                                                                                               Just fig -> snd fig == color
-                                              remove pos = haveToBeat pos && unableToBeat pos
-                                              keep pos = not $ remove pos
-                                          in  filter keep moves
+removeReqBeating board color (fromX, fromY) moves = let haveToBeat (x, y) = abs (x - fromX) == 2
+                                              		yMod (x, y)= if y > fromY then 1 else -1
+                                              		beatPos (x, y) = if x > fromX then (x - 1, y - yMod (x, y))
+                                                                            	      else (x + 1, y - yMod (x, y))
+                                              		unableToBeat pos = case (Data.Map.lookup (beatPos pos) board) of Nothing -> True
+                                                                                                               		 Just fig -> snd fig == color
+                                              		remove pos = haveToBeat pos && unableToBeat pos
+                                              		keep pos = not $ remove pos
+                                          	    in  filter keep moves
 
 removeInvalidMoves board color from moves = removeReqBeating board color from $ removeOccupied board $ removeOffBoard moves
 
@@ -89,3 +97,5 @@ nodesAtLevel n (Node _ subtrees) = concatMap (nodesAtLevel (n-1)) subtrees
 
 b = stringToBoard initBoard
 --m = removeOffBoard $ generateMoves (1,2) Black
+testBoard1 = ".b.b.b.b\nb.b.b.b.\n.b.b.w.b\n........\n........\nw.w.w.w.\n.w.w.w.w\nw.w.w.w."
+tb1 = stringToBoard testBoard1
